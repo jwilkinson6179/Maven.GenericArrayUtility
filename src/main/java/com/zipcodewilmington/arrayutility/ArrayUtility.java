141ee99e016cf2a7c3fb6e844892293c2829974a
James Wilkinson <jwilkinson@James-Wilkinsons-MacBook-Pro.local>
2020-03-07 18:12:32 -0500

    Type erasure; never figured this out on assessment 1

--- src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
+++ src/main/java/com/zipcodewilmington/arrayutility/ArrayUtility.java
@@ -3,5 +3,41 @@ package com.zipcodewilmington.arrayutility;
 /**
  * Created by leon on 3/6/18.
  */
-public class ArrayUtility {
+public class ArrayUtility<T> {
+    T[] array;
+
+    public ArrayUtility(T[] array) {
+        this.array = array;
+    }
+
+    public Integer getNumberOfOccurrences(T valueToEvaluate)
+    {
+        Integer occurrences = 0;
+
+        for(T element : array)
+        {
+            if(element.equals(valueToEvaluate))
+            {
+                occurrences++;
+            }
+        }
+
+        return occurrences;
+    }
+
+    public Integer countDuplicatesInMerge(T[] arrayToMerge, T valueToEvaluate)
+    {
+        return null;
+    }
+
+    public T getMostCommonFromMerge(T[] arrayToMerge)
+    {
+        return null;
+    }
+
+    public T[] removeValue(T valueToRemove)
+    {
+//        T[] = new T[0];
+        return array;
+    }
 }
